28858b1390b591c8ca7a88d96199eee24d9755bf
Christoph Kappel <christoph@unexist.dev>
2021-05-05 18:38:57 +0200

    Renamed to TodoTransformer

--- debezium-transformer/src/main/java/dev/unexist/showcase/transformer/TodoTransformer.java
+++ debezium-transformer/src/main/java/dev/unexist/showcase/transformer/TodoTransformer.java
@@ -9,36 +9,36 @@ import org.apache.kafka.connect.transforms.Transformation;
 
 import java.util.Map;
 
-/**
- * The class is configured and invoked when a change occurs on any outbox
- **/
-public class CustomTransformer<R extends ConnectRecord<R>> implements Transformation<R> {
+/*The class is configured and invoked when a change occurs on any outbox */
+public class TodoTransformer<R extends ConnectRecord<R>> implements Transformation<R> {
 
     /**
      * This method is invoked when a change is made on the outbox schema.
      *
      * @param sourceRecord
+     *          Source record to handle
      * @return
-     */
+     **/
+
     public R apply(R sourceRecord) {
 
         Struct kStruct = (Struct) sourceRecord.value();
         String databaseOperation = kStruct.getString("op");
 
-        //Handle only the Create's
+        /* Handle only the Create's */
         if ("c".equalsIgnoreCase(databaseOperation)) {
 
-            // Get the details.
+            /* Get the details */
             Struct after = (Struct) kStruct.get("after");
             String UUID = after.getString("uuid");
-            String payload = after.getString("payload");
             String eventType = after.getString("event_type").toLowerCase();
             String topic = eventType.toLowerCase();
+            String payload = after.getString("payload");
 
             Headers headers = sourceRecord.headers();
             headers.addString("eventId", UUID);
 
-            // Build the event to be published.
+            /* Build the event to be published */
             sourceRecord = sourceRecord.newRecord(topic, null,
                     Schema.STRING_SCHEMA, UUID, null, payload,
                     sourceRecord.timestamp(), headers);
